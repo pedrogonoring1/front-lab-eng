f6b3eccccbc0b73ba9d1dc2737c89bbb6fb75c8a
IcaroLR01 <icaro.leite@hotmail.com>
2022-09-25 16:41:06 -0300

    Cria tela de loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Register, RecoverPassword, Login } from '../screens'
+import { Register, RecoverPassword, Login, Loading } from '../screens'
 
 export type RootStackParamList = {
+  Loading: undefined
   Login: undefined
   Register: undefined
   RecoverPassword: undefined
@@ -15,6 +16,7 @@ export const Routes = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Loading' component={Loading} />
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Register' component={Register} />
         <Stack.Screen name='RecoverPassword' component={RecoverPassword} />
